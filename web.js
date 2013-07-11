d58f02e9a77b517f28bf10bdc92b6db5194335f2
Ubuntu <f.vinti@gmail.com>
2013-07-11 08:11:52 +0000

    Modified the web.js to load data from file

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var fs = require('fs');
+var Buffer = require('buffer').Buffer;
 
 var app = express.createServer(express.logger());
 
+
+var readBuf = new Buffer(100); 
+fs.readFileSync('index.html', readBuf, function (err, data) {
+    if (err) throw err;
+    //console.log(readBuf.toString('utf-8', 0, data));
+    console.log(readBuf.toString);
+});
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readBuf);
 });
 
 var port = process.env.PORT || 5000;
